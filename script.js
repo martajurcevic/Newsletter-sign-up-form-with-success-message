8697b2a80e6534f962800526876146e620899391
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-10-04 13:17:24 +0200

    first commit

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
-const doc = document;
+const main = document.getElementById('main')
+const modal = document.getElementById('modal')
 
-const isValidEmail = (email) => {
-  const regex =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-  return regex.test(email);
-};
+const emailInput = document.getElementById('emailInput');
+const subscribeButton = document.getElementById('subscribeToNewsletter')
+const dismissButton = document.getElementById('dismissButton')
+const emailWarning = document.getElementById('email-warning')
 
-const $emailInput = doc.getElementById("email");
-const $errorMessage = doc.getElementById("error-message");
-const $newsletterForm = doc.getElementById("newsletter-form");
-let touched = false;
-let validEmail = false;
+const userMail = document.getElementById('userMail')
 
-const showErrorMessage = () => {
-  const value = $emailInput.value.trim(); // Trim whitespace from input
-  const isValid = isValidEmail(value);
+// Subscribe to newsletter
+subscribeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = emailInput.value.trim()
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email) || !email) {
+        emailInput.classList.add('invalid');
+        emailWarning.removeAttribute('hidden')
+    } else {
+        emailInput.classList.remove('invalid');
+        emailWarning.setAttribute('hidden', '')
+        userMail.textContent = emailInput.value.trim()
+        main.classList.add('hidden')
+        modal.classList.remove('hidden')
+    }
+})
 
-  $errorMessage.classList.toggle("error-message--hidden", isValid || !touched);
-  $emailInput.classList.toggle(
-    "newsletter__email-input--error",
-    !isValid && touched
-  );
-
-  validEmail = isValid;
-};
-
-$emailInput.addEventListener("input", () => {
-  touched = true;
-  showErrorMessage();
-});
-
-$newsletterForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!validEmail) {
-    alert("The email is not valid.");
-  } else {
-    $emailInput.value = "";
-    window.location.href = "success.html";
-  }
-});
+// Dismiss message in modal
+dismissButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    main.classList.remove('hidden');
+    modal.classList.add('hidden');
+    emailInput.value = ''
+})
